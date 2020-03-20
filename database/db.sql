26c8f1426bb0739d08dd9213b5a7d97b70a1a7c5
lipedm <57644779+lipedm@users.noreply.github.com>
2020-03-20 20:25:51 -0300

    Add files via upload

---
+++ database/db.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v12.09 (32 bit)
+MySQL - 10.4.11-MariaDB : Database - saipos
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`saipos` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `saipos`;
+
+/*Table structure for table `lista_todo` */
+
+DROP TABLE IF EXISTS `lista_todo`;
+
+CREATE TABLE `lista_todo` (
+  `id_reg` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome_responsavel` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `descricao` blob DEFAULT NULL,
+  `situacao` enum('P','C') DEFAULT 'P' COMMENT 'P para Pendente, C para Concluída',
+  `qtd_conc` int(1) DEFAULT 0,
+  `data_criacao` datetime DEFAULT current_timestamp(),
+  `data_baixa` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_reg`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin7;
+
+/*Data for the table `lista_todo` */
+
+insert  into `lista_todo`(`id_reg`,`nome_responsavel`,`email`,`descricao`,`situacao`,`qtd_conc`,`data_criacao`,`data_baixa`) values (5,'Felipe','felipe@teste.com','teste','C',0,'2020-03-20 17:24:28',NULL),(6,'Amanda','amanda@atest.com','lorem ipsum','C',0,'2020-03-20 17:49:06',NULL),(7,'Amilton','amuilton@asdokaos.com','Lavar a roupa','P',0,'2020-03-20 17:58:01',NULL),(8,'Daniela','dani@dani.com','qwe','C',0,'2020-03-20 17:58:50',NULL),(9,'Geanine','geaninepavan@gmail.com','Chegar em casa\nDar comida pros cachorros\nFazer janta','P',0,'2020-03-20 19:21:58',NULL);
+
+/*Table structure for table `log_lista` */
+
+DROP TABLE IF EXISTS `log_lista`;
+
+CREATE TABLE `log_lista` (
+  `id_reg` int(11) NOT NULL,
+  `id_lista` int(4) DEFAULT NULL,
+  `log` blob DEFAULT NULL,
+  PRIMARY KEY (`id_reg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `log_lista` */
+
+/*Table structure for table `sys_supervisor` */
+
+DROP TABLE IF EXISTS `sys_supervisor`;
+
+CREATE TABLE `sys_supervisor` (
+  `id_reg` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_supervisor` varchar(20) DEFAULT NULL,
+  `psw` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id_reg`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_supervisor` */
+
+insert  into `sys_supervisor`(`id_reg`,`nome_supervisor`,`psw`) values (1,'Felipe','20f6a9308ce0977dd5d4f9b3a3f9a4bf');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

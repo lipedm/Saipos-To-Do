03f113e85e1797ed3508b45d504e8a5215db9c5f
Felipe Fernandes <57644779+lipedm@users.noreply.github.com>
2020-11-28 01:48:33 -0300

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (32 bit)
 MySQL - 10.4.11-MariaDB : Database - saipos
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,7 +13,7 @@ MySQL - 10.4.11-MariaDB : Database - saipos
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`saipos` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `saipos`;
 
